8dccfce5e5fdc3d828bf7eab23d7c5be784e272a
IMRAAN <112625365+IMRAAN012@users.noreply.github.com>
2022-10-14 10:26:40 +0530

    Add files via upload

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material"
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus sodales egestas dolor amet aliquam. Blandit non nibh congue bibendum amet, leo. Eu accumsan, sit vitae, vel dictum auctor tortor sit. Eros amet, eu mi lorem lacinia euismod. Eu pulvinar arcu, sed euismod elit nisl, etiam ultrices. Id ut lacus, vulputate ut quisque bibendum. Id sed sed et tristique leo in. Mattis molestie velit lectus lobortis cursus enim. Ultricies lectus elementum rhoncus vivamus vel. Purus egestas egestas at facilisis nullam non gravida.
+                Commodo ut morbi sed adipiscing sollicitudin consequat, in. Quam viverra pellentesque lorem suspendisse pulvinar lectus non odio donec. Ut integer vitae feugiat justo. Facilisis eu, donec molestie gravida consequat. Eu in eu purus sit dolor faucibus enim sed. Lectus eros, luctus in diam tortor enim in ipsum vitae. Diam in ut proin adipiscing integer praesent ut semper adipiscing. Diam aliquet scelerisque mauris in aliquet eu sit vulputate sapien. Velit, convallis mi praesent nulla leo urna, vulputate diam. At a vivamus mauris consectetur mattis non interdum tristique. Viverra nulla eget pharetra, sagittis, ullamcorper. Lorem nunc tellus nunc, eget ullamcorper neque tellus. Mattis cras ac gravida nec arcu. Id donec tincidunt enim orci.
+            </Typography>
+            
+        </div>
+    )
+}
